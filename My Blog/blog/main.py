9a5556eedd66c4a648deffea7ec946706cfcfabd
Aaqil Modak <82268834+aaqilmodak29@users.noreply.github.com>
2021-10-08 11:39:10 +0530

    Update main.py

--- My Blog/blog/main.py
+++ My Blog/blog/main.py
@@ -73,7 +73,7 @@ class BlogPost(db.Model, Base):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(1000), nullable=False)
 
     comments = relationship("Comment", back_populates="parent_post")
 
